c8dc6c1af986e32a4716b4e18ed2071788bc700e
Carlos Mauricio Mesa Cantillo <carlos_mesa_98@gmail.com>
2020-02-12 11:05:35 +0000

    Documentacion inicial

--- app/src/main/java/com/gmail/carlosmesac98/myapplication/MainActivity.java
+++ app/src/main/java/com/gmail/carlosmesac98/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//Documentacin inicial
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
